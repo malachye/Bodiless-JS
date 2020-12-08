68e9f6ef57c8e70b602480760bf4745aed36496e
Beliayeu, Andrei [JJCUS Non-J&J] <abeliaye@ITS.JNJ.com>
2020-12-08 14:10:59 +0300

    poc paste html link to richtext

---
+++ packages/bodiless-richtext/src/withHtmlPaste.tsx
@@ -0,0 +1,68 @@
+/**
+ * Copyright © 2020 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Transforms } from 'slate'
+import { jsx } from 'slate-hyperscript';
+
+const ELEMENT_TAGS = {
+  A: el => ({ type: 'Link', data: { slatenode: { href: el.getAttribute('href') } } }),
+  H1: () => ({ type: 'H1' }),
+};
+
+export const deserialize = el => {
+  if (el.nodeType === 3) {
+    return el.textContent
+  } else if (el.nodeType !== 1) {
+    return null
+  }
+
+  const { nodeName } = el
+  let parent = el
+
+  const children = Array.from(parent.childNodes)
+    .map(deserialize)
+    .flat()
+
+  if (el.nodeName === 'BODY') {
+    return jsx('fragment', {}, children)
+  }
+
+  if (ELEMENT_TAGS[nodeName]) {
+    const attrs = ELEMENT_TAGS[nodeName](el)
+    return jsx('element', attrs, children)
+  }
+  
+  return children
+}
+
+const withHtmlPaste = editor => {
+  const { insertData } = editor
+
+  editor.insertData = data => {
+    const html = data.getData('text/html')
+
+    if (html) {
+      const parsed = new DOMParser().parseFromString(html, 'text/html')
+      const fragment = deserialize(parsed.body)
+      Transforms.insertFragment(editor, fragment)
+      return
+    }
+
+    insertData(data)
+  }
+
+  return editor
+}
+
+export default withHtmlPaste;
